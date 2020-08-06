ff50480682f33a5f127c0512f7c346c42dcd5398
antariess <velislava.n.georgieva@gmail.com>
2020-08-06 09:59:07 +0100

    initial set up

---
+++ db/data/images.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {
+    image_url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQkoaGzAOdNsaGqg1iqxk8gfxD8Bur9eiAYg&usqp=CAU",
+    category: "cat",
+    title: "cat",
+  },
+];
